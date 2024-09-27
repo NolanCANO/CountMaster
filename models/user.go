8a82ebdb2de2066ab078adb242f6eb0fa9661aad
NCANO <nolan.cano@ynov.com>
2024-09-27 02:44:22 +0200

    modif model user

--- models/user.go
+++ models/user.go
@@ -5,10 +5,10 @@ import (
 )
 
 type User struct {
-	ID           uint      `gorm:"primaryKey"`
+	ID           uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	Username     string    `gorm:"unique;not null" json:"username"`
 	Email        string    `gorm:"unique;not null" json:"email"`
-	PasswordHash string    `gorm:"not null;default:'default_password_hash'" json:"-"`
+	PasswordHash string    `gorm:"not null" json:"-"`
 	Salary       float64   `gorm:"default:0" json:"salary"`
 	CreatedAt    time.Time `json:"created_at"`
 	Groups       []Group   `gorm:"many2many:group_users" json:"groups"`
